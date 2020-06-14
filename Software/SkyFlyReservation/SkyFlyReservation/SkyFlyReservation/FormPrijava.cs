b3fb75f219fa5ba7201a033594c475314802be8a
Dario Alilović <dalilovic@foi.hr>
2020-06-14 23:51:09 +0200

    Prijava i autentikacija
    Implementacija mogućnosti prijave i autentikacije korisnika u sustav. Trenutno redirect ide na popis rezervacija prijavljenog korisnika.

---
+++ Software/SkyFlyReservation/SkyFlyReservation/SkyFlyReservation/FormPrijava.cs
@@ -0,0 +1,76 @@
+﻿using SkyFlyReservation.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkyFlyReservation
+{
+    public partial class FormPrijava : Form
+    {
+        bool SeePassword = false;
+        bool Autentikacija = false;
+        public FormPrijava()
+        {
+            InitializeComponent();
+            SetupPasswordTextbox();
+        }
+
+        private void SetupPasswordTextbox()
+        {
+            txtLozinka.PasswordChar = '•';
+        }
+
+        private void PrijaviSeButton_Click(object sender, EventArgs e)
+        {
+            string KorisnickoIme = txtKorIme.Text;
+            string Lozinka = txtLozinka.Text;
+
+            List<Korisnik> korisnici = RepozitorijSkyFlyReservation.DohvatiSveKorisnike();
+            Korisnik korisnik = new Korisnik();
+            foreach (var item in korisnici)
+            {
+                if(KorisnickoIme == item.KorisnickoImeKorisnika && Lozinka == item.LozinkaKorisnika)
+                {
+                    Autentikacija = true;
+                    korisnik = item;
+                }
+            }
+
+            if(Autentikacija == true)
+            {
+                RepozitorijSkyFlyReservation.AutenticirajKorisnika(korisnik);
+                FormPregledRezervacija form = new FormPregledRezervacija();
+                form.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Korisnik sa navedenim podacima ne postoji!");
+            }
+        }
+
+        private void btnSeePassword_Click(object sender, EventArgs e)
+        {
+            if (SeePassword)
+            {
+                txtLozinka.PasswordChar = '•';
+                SeePassword = false;
+            }
+            else
+            {
+                txtLozinka.PasswordChar = '\0';
+                SeePassword = true;
+            }
+        }
+
+        private void btnNatrag_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
