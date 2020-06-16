47ed5d5a6bfbd6d86f2d5059215d30fcc719e8e2
Zlatko S <zlatko.stapic@foi.hr>
2020-06-16 20:32:56 +0200

    Spajanje pgalina verzije u development.

--- Software/SkyFlyReservation/SkyFlyReservation/Program.cs
+++ Software/SkyFlyReservation/SkyFlyReservation/Program.cs
@@ -16,7 +16,7 @@ namespace SkyFlyReservation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormPrijava());
+            Application.Run(new FormPregledLetova());
         }
     }
 }
